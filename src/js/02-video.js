583f7a649f16df5b27a83d6bf9580889ba1c1546
wieslawbobieni <bk.bobieni@gmail.com>
2023-05-15 09:43:26 +0200

    task2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,15 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+let timeOfVideo = localStorage.getItem('videoplayer-current-time');
+if (timeOfVideo !== null) {
+  player.setCurrentTime(timeOfVideo);
+}
+player.on('timeupdate', throttle(currentTimeOfVideo, 1000));
+function currentTimeOfVideo() {
+  player
+    .getCurrentTime()
+    .then(seconds => localStorage.setItem('videoplayer-current-time', seconds));
+}
